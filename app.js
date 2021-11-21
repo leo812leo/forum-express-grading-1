c19c4b1a12f340ed90d21189bebd0af3fbb4c347
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-21 14:48:48 +0800

    U99 sequelize initialize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const { engine } = require('express-handlebars');
+const { engine } = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const port = 3000
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
