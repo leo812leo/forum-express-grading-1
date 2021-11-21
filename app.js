bce1fc9c9df9407ec4c6e6851e27938283d956ff
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-21 14:35:16 +0800

    U97 initialize

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const { engine } = require('express-handlebars');
 const port = 3000
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
