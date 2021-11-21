8df0eb08b9f0f2766ba6a789363e356d4e567703
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-21 17:22:22 +0800

    U105

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,15 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-
+  signUp: (req, res) => {
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+    }).then(user => {
+      return res.redirect('/signin')
+    })
+  },
   signInPage: (req, res) => {
     return res.render('signin')
   },
