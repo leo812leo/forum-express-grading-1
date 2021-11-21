12103f00f3647d01d2747f95fcc8b860b5888b6d
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-21 16:02:41 +0800

    U102

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const { engine } = require('express-handlebars')
 const db = require('./models') // 引入資料庫
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
-app.use(express.urlencoded({ extended: true }))
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
