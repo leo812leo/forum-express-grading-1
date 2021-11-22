2325b02fcc1a3345c05d5d11d2e7dafb0fe836ef
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-22 23:45:25 +0800

    U143

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,24 +1,52 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: async (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
 
-    const restaurants = await Restaurant.findAll({ include: Category, where: whereQuery })
-    const data = restaurants.map(r => ({
+    const result = await Restaurant.findAndCountAll({
+      include: Category,
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    })
+    // data for pagination
+    const page = Number(req.query.page) || 1
+    const pages = Math.ceil(result.count / pageLimit)
+    const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+    const prev = page - 1 < 1 ? 1 : page - 1
+    const next = page + 1 > pages ? pages : page + 1
+    // clean up restaurant data
+    const data = result.rows.map(r => ({
       ...r.dataValues,
       description: r.dataValues.description.substring(0, 50),
       categoryName: r.Category.name
     }))
+    //categories
     const categories = await Category.findAll({ raw: true, nest: true })
-    return res.render('restaurants', { restaurants: data, categories, categoryId })
+    //render
+    return res.render('restaurants', {
+      restaurants: data,
+      categories,
+      categoryId,
+      page,
+      totalPage,
+      prev,
+      next
+    })
   },
   getRestaurant: async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id, { include: Category })
