c7f68c122a8ffe1696673fc50c616c08afd105e6
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-22 22:57:36 +0800

    U140

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,17 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    const restaurants = await Restaurant.findAll({ include: Category })
+    const data = restaurants.map(r => ({
+      ...r.dataValues,
+      description: r.dataValues.description.substring(0, 50),
+      categoryName: r.Category.name
+    }))
+    return res.render('restaurants', { restaurants: data })
   }
 }
+
 module.exports = restController
