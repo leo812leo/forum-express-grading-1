79f6c67d299acfbd70b8956acfe60ed4f37daccb
leo812leo <leo_812leo@hotmail.com.tw>
2021-11-22 23:25:46 +0800

    U142

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,18 +4,26 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: async (req, res) => {
-    const restaurants = await Restaurant.findAll({ include: Category })
+    const whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.CategoryId = categoryId
+    }
+
+    const restaurants = await Restaurant.findAll({ include: Category, where: whereQuery })
     const data = restaurants.map(r => ({
       ...r.dataValues,
       description: r.dataValues.description.substring(0, 50),
       categoryName: r.Category.name
     }))
-    return res.render('restaurants', { restaurants: data })
+    const categories = await Category.findAll({ raw: true, nest: true })
+    return res.render('restaurants', { restaurants: data, categories, categoryId })
   },
   getRestaurant: async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id, { include: Category })
     return res.render('restaurant', { restaurant: restaurant.toJSON() })
-  }
+  },
 }
 
 module.exports = restController
